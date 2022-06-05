83e10638fa53675877848f383d0ef5fc7e3e9d0b
Alif Delgado <alifdelgado@gmail.com>
2022-06-04 20:49:23 -0500

    CRUD methods were added to the user controller and the user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    const user = this.userService.findOne(+id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.userService.create(body.email, body.password);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(+id, body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(+id);
   }
 }
